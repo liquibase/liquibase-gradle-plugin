235a3f3bb51ee3636f581719805ee17bc0921aea
nouranatef <engineer_nouran@yahoo.com>
2025-08-26 17:28:38 +0300

    Simplify `ProjectInfo` by removing unused fields

--- src/main/groovy/org/liquibase/gradle/ProjectInfo.groovy
+++ src/main/groovy/org/liquibase/gradle/ProjectInfo.groovy
@@ -6,34 +6,17 @@ class ProjectInfo {
     List<Activity> activities
     String runList
     List<String> jvmArgs
-    Map<String, Object> liquibaseProperties
-    File buildDir
-    Object logger
 
-    ProjectInfo(List<Activity> activities, String runList, List<String> jvmArgs, Map<String, Object> liquibaseProperties, File buildDir, Object logger) {
+    ProjectInfo(List<Activity> activities, String runList, List<String> jvmArgs) {
         this.activities = activities
         this.runList = runList
         this.jvmArgs = jvmArgs
-        this.liquibaseProperties = liquibaseProperties
-        this.buildDir = buildDir
-        this.logger = logger
     }
 
     static ProjectInfo fromProject(Project project) {
-        def buildDir = project.buildDir
-        def logger = project.logger
-        def liquibaseProperties = [:]
-        project.properties.findAll { key, value ->
-            if (!key.startsWith("liquibase")) return false
-            if (value != null && LiquibaseTask.class.isAssignableFrom(value.class)) return false
-            return true
-        }.each { key, value ->
-            liquibaseProperties[key] = value
-        }
         def activities = project.liquibase.activities.toList()
         def runList = project.liquibase.runList
         def jvmArgs = project.liquibase.jvmArgs
-        return new ProjectInfo(activities, runList, jvmArgs,
-                liquibaseProperties as Map<String, Object>, buildDir, logger)
+        return new ProjectInfo(activities, runList, jvmArgs)
     }
 }
